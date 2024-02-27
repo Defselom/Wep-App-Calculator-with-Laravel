d6c1fe45ae472cd3b9c4eac8aae971156ee74ab7
defselom <selomadzomla@gmail.com>
2024-02-27 08:10:09 +0000

    feat: routing for pages done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('page1');
 });
+
+Route::get('/page2', function () {
+    return view('page2');
+});
+
+Route::get('/page3', function () {
+    return view('page3');
+});
