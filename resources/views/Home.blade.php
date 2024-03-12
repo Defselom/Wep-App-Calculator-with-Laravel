2a0d73bd2d318ec84c4b2906d297a7091aabf90b
defselom <selomadzomla@gmail.com>
2024-03-12 09:03:37 +0000

    feat: home page view

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,12 @@
+@extends('home')
+
+
+@section('content')
+<div class="">
+    <h2 class="text-center w-full font-bold">Home Page</h2>
+<div class="w-full text-center">
+   An Calculator Web App
+</div>
+</div>
+
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -106,7 +106,7 @@
   
   <div class="p-4 sm:ml-64">
      <div class="p-4 border-2 min-h-screen border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-        @yield('content')
+      @yield('content')
      </div>
   </div>
   
