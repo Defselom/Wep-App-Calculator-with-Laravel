3e0456b5c290060590dea8a2fd72371978037eea
defselom <selomadzomla@gmail.com>
2024-03-13 08:10:28 +0000

    feat: all operation component implementation done

--- resources/views/Addition.blade.php
+++ resources/views/Addition.blade.php
@@ -4,12 +4,11 @@
 @section('content')
 <div class="">
     <h2 class="text-center w-full font-bold">Addition</h2>
+</div>
 <div>
 
   <x-form action="{{ route('makeAddition')}}" method='get' number1='$number1' number2='$number2' operation='Addition' ></x-form>
 
-    
-</div>
 </div>
 
 @endsection
